e48521e152510c0cccc4747b54089c52984b5b86
Wenson Tsai <wensontsai@gmail.com>
2015-10-31 17:04:47 -0400

    old skool shit

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,23 @@
 var React = require('react');
 var ReactRouter = require('react-router');
-var HashHistory = require('react-router/lib/hashhistory');
+var HashHistory = require('react-router/lib/HashHistory');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Hello = React.createClass({
   render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
+    return (
+    	<h1 className="red">
+      		{this.props.children}
+    	</h1>
+    )
   }
 });
 
 var Child1 = React.createClass({
 	render: function(){
 		return(
-			return <h1>I'm Child1.</h1>
+			<h1>I'm Child1.</h1>
 		);
 	}
 });
@@ -23,10 +25,19 @@ var Child1 = React.createClass({
 var Child2 = React.createClass({
 	render: function(){
 		return(
-			return <h1>I'm the other child, Child2.</h1>
+			<h1>I'm the other child, Child2.</h1>
 		);
 	}
 });
 
-var element = React.createElement(Hello, {});
-React.render(element, document.querySelector('.container'));
+var routes = (
+	<Router history={new HashHistory}>
+		<Route path="/" component={Hello}>
+			<Route path="1" component={Child1} />
+			<Route path="2" component={Child2} />
+		</Route>
+	</Router>
+);
+
+// var element = React.createElement(Hello, {});
+React.render(routes, document.querySelector('.container'));
